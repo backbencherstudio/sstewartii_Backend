1935751bb5a6d132b03186bb122befc00722f4b3
sahadat96 <sahadatappdev@gmail.com>
2026-05-13 16:41:45 +0600

    creted queue.constants.ts file

---
+++ src/common/queues/queue.constants.ts
@@ -0,0 +1,7 @@
+export const AUTH_QUEUE = 'auth-queue';
+
+export const SEND_EMAIL_VERIFICATION_OTP_JOB =
+  'send-email-verification-otp';
+
+export const SEND_PASSWORD_RESET_OTP_JOB =
+  'send-password-reset-otp';
